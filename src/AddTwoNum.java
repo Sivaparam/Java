a05cd34ff34175f144967e3049073560c306087c
Sivaranjani Jayaprakash <ranjani@SivaranjanisMBP.attlocal.net>
2022-12-29 09:44:46 -0500

    Initial commit

---
+++ src/AddTwoNum.java
@@ -0,0 +1,14 @@
+public class AddTwoNum {
+    int sum(int x, int y){
+        int result = x+y;
+        return result;
+    }
+    public static void main (String[] args) {
+        int x = 5;
+        int y = 5;
+        AddTwoNum obj1 = new AddTwoNum();
+        int result = obj1.sum(x,y);
+        System.out.println("Sum is:" +result);
+
+    }
+}
